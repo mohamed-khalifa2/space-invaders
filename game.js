04fd711bb7f09f1e859d483601cdf97596fb84df
mohamed-khalifa2 <runeaaberg1@gmail.com>
2025-04-19 03:18:24 +0200

    init

---
+++ game.js
@@ -0,0 +1,66 @@
+
+const right = document.querySelector('.right');
+const left = document.querySelector('.left');
+const fire = document.querySelector('.fire');
+
+
+right.addEventListener('click', () => {
+    const event = new KeyboardEvent('keydown', {
+        key: 'ArrowRight',
+        code: 'ArrowRight',
+        keyCode: 39,
+        which: 39,
+        bubbles: true,
+    });
+    document.dispatchEvent(event);
+    setTimeout(() => {
+        const keyUpEvent = new KeyboardEvent('keyup', {
+            key: 'ArrowRight',
+            code: 'ArrowRight',
+            keyCode: 39,
+            which: 39,
+            bubbles: true
+        });
+        document.dispatchEvent(keyUpEvent);
+    }, 100); // 100ms delay (adjust as needed)
+});
+left.addEventListener('click', () => {
+    const event = new KeyboardEvent('keydown', {
+        key: 'ArrowLeft',
+        code: 'ArrowLeft',
+        keyCode: 37,
+        which: 37,
+        bubbles: true,
+    });
+    document.dispatchEvent(event);
+    setTimeout(() => {
+        const keyUpEvent = new KeyboardEvent('keyup', {
+            key: 'ArrowLeft',
+            code: 'ArrowLeft',
+            keyCode: 37,
+            which: 37,
+            bubbles: true
+        });
+        document.dispatchEvent(keyUpEvent);
+    }, 100); // 100ms delay (adjust as needed)
+});
+fire.addEventListener('click', () => {
+    const event = new KeyboardEvent('keydown', {
+        key: 'Space',
+        code: 'Space',
+        keyCode: 32,
+        which: 32,
+        bubbles: true,
+    });
+    document.dispatchEvent(event);
+    setTimeout(() => {
+        const keyUpEvent = new KeyboardEvent('keyup', {
+            key: 'Space',
+            code: 'Space',
+            keyCode: 32,
+            which: 32,
+            bubbles: true
+        });
+        document.dispatchEvent(keyUpEvent);
+    }, 100); // 100ms delay (adjust as needed)
+});
